bae47a3f48a4a221ec06fb0aca941ab62fa3d10a
Doriss310 <dqdung310@gmail.com>
2024-12-09 14:45:13 +0700

    Update FullImage

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -17,31 +17,60 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
-
-  void removeItemFromCart(){
+  void removeItemFromCart() {
     Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-      borderRadius: BorderRadius.circular(8),
-      ),
-      margin: const EdgeInsets.only(bottom: 10),
-      child: ListTile(
-        leading: SizedBox(
-          width: 100,
-          height: 100,
-          child: Image.asset(
-            widget.shoe.image,
-            fit: BoxFit.fill,
+    return Consumer(
+      builder: (context, value, child) => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.only(bottom: 10),
+        child: ListTile(
+          leading: GestureDetector(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context,) {
+                    return AlertDialog(
+                      insetPadding: EdgeInsets.zero,
+                      backgroundColor: Colors.transparent,
+                      content: GestureDetector(
+                        onTap: (){
+                          Navigator.pop(context);
+                        },
+                        child: SizedBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: Image.asset(
+                            widget.shoe.image,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    );
+                  },);
+            },
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset(
+                widget.shoe.image,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          title: Text(widget.shoe.name),
+          subtitle: Text(widget.shoe.cost),
+          trailing: IconButton(
+            onPressed: removeItemFromCart,
+            icon: const Icon(Icons.delete),
           ),
         ),
-        title: Text(widget.shoe.name),
-        subtitle: Text(widget.shoe.cost),
-        trailing: IconButton(onPressed: removeItemFromCart, icon: const Icon(Icons.delete),),
       ),
     );
   }
